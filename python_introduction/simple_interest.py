f5acb395300f74206be97d93a11c6bcd821a6b8f
iradukunda murenzi cherubin <cherubinbond@gmail.com>
2024-06-30 17:53:59 +0200

    added python_introduction

--- python_introduction/simple_interest.py
+++
@@ -1,8 +0,0 @@
-# variables
-principal = 1000
-rate = 0.005
-time = 3
-# calculation
-interest = principal * rate * time
-# results
-print("the simple interest is {interest}")
---
+++ python_introduction/simple_interst.py
@@ -0,0 +1,7 @@
+#variables
+principal = 1000
+rate = 0.05
+time = 3
+#calculation
+interest = principal * rate * time 
+print ("the simple interest is: {interest}")
